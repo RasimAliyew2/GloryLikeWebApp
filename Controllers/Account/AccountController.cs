3c16b661ab961f3d676340735ff5a8bd0b127f54
Aliyew Rasim <77281967+RasimAliyew2@users.noreply.github.com>
2026-07-16 01:23:50 +0400

    First commit

---
+++ Controllers/Account/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using GloryLikeWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GloryLikeWebApp.Controllers.Account
+{
+    public class AccountController : Controller
+    {
+
+        [HttpGet("/SignIn")]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        [HttpPost("/SignIn")]
+        public IActionResult SignIn(UserInfo user)
+        {
+            return View();
+        }
+    }
+}
